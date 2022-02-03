83a0895da39af250842adf778695dfadf5353778
Helton M. Pereira <heltonmpereira@hotmail.com>
2022-02-02 23:59:24 -0300

    Criação dos modulos de edição e rotinas iniciais para montagem das recorrencias

--- src/MinhaCarteira.Cliente.AppWebMvc/Controllers/AgendamentoController.cs
+++ src/MinhaCarteira.Cliente.AppWebMvc/Controllers/AgendamentoController.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Dates.Recurring;
+using Dates.Recurring.Builders;
+using Dates.Recurring.Type;
 using Microsoft.AspNetCore.Mvc;
 using MinhaCarteira.Cliente.AppWebMvc.Controllers.Base;
 using MinhaCarteira.Cliente.AppWebMvc.Models;
 using MinhaCarteira.Cliente.Recursos.Refit.Base;
 using MinhaCarteira.Comum.Definicao.Entidade;
+using System;
 using System.Threading.Tasks;
 
 namespace MinhaCarteira.Cliente.AppWebMvc.Controllers
@@ -15,14 +19,69 @@ namespace MinhaCarteira.Cliente.AppWebMvc.Controllers
             IMapper mapper)
             : base(servico, mapper) { }
 
+        private RecurrenceType ObterRecorrenciaBuilder(AgendamentoViewModel viewModel)
+        {
+            switch (viewModel.TipoRecorrencia)
+            {
+                case Comum.Definicao.Modelo.TipoRecorrencia.Semanal:
+                    return Recurs
+                        .Starting(viewModel.DataInicial)
+                        .Every(viewModel.Parcelas)
+                        .Weeks()
+                        .Ending(viewModel.DataFinal ?? viewModel.DataInicial)
+                        .Build();
+                case Comum.Definicao.Modelo.TipoRecorrencia.Mensal:
+                    return Recurs
+                        .Starting(viewModel.DataInicial)
+                        .Every(viewModel.Parcelas)
+                        .Months()
+                        .Ending(viewModel.DataFinal ?? viewModel.DataInicial)
+                        .Build();
+                case Comum.Definicao.Modelo.TipoRecorrencia.Anual:
+                    return Recurs
+                        .Starting(viewModel.DataInicial)
+                        .Every(viewModel.Parcelas)
+                        .Years()
+                        .Ending(viewModel.DataFinal ?? viewModel.DataInicial)
+                        .Build();
+                default:
+                    return Recurs
+                        .Starting(viewModel.DataInicial)
+                        .Every(viewModel.Parcelas)
+                        .Days()
+                        .Ending(viewModel.DataFinal ?? viewModel.DataInicial)
+                        .Build();
+            }
+        }
+
         protected override async Task<AgendamentoViewModel> InicializarViewModel(AgendamentoViewModel viewModel)
         {
             return await Task.FromResult(viewModel);
         }
-
         protected override async Task<bool> ValidarViewModel(AgendamentoViewModel viewModel)
         {
             return await Task.FromResult(true);
+        }
+        protected override async Task<Tuple<AgendamentoViewModel, Agendamento>> ExecutarAntesSalvar(AgendamentoViewModel viewModel, Agendamento model)
+        {
+            var agendamento = ObterRecorrenciaBuilder(viewModel);
+            DateTime data = viewModel.DataInicial;
+            DateTime? proximo;
+
+            do
+            {
+                viewModel.AdicionarParcela(data);
+                
+                proximo = agendamento.Next(data);
+                if (proximo.HasValue)
+                    data = new DateTime(
+                        proximo.Value.Year,
+                        proximo.Value.Month,
+                        proximo.Value.Day);
+
+            } while (proximo != null);
+
+            return await base.ExecutarAntesSalvar(viewModel, model);
         }
 
         #region Métodos sobrescritos apenas manter as views
---
+++ src/MinhaCarteira.Comum.Definicao/Modelo/TipoParcelas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaCarteira.Comum.Definicao.Modelo
+{
+    public enum TipoParcelas
+    {
+        Unica,
+        Parcelada,
+        Recorrente
+    }
+}
