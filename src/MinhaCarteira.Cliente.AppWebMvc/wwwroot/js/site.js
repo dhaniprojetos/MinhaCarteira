e43f84395cd7d82e802b84d325a433d83185de2d
Helton M. Pereira <hempmax@msn.com>
2022-03-15 15:12:45 -0300

    Correçõs nos caminhos dos arquivos

--- src/MinhaCarteira.Cliente.AppWebMvc/wwwroot/js/site.js
+++ src/MinhaCarteira.Cliente.AppWebMvc/wwwroot/js/site.js
@@ -29,6 +29,6 @@ $(document).ready(function () {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/js/service-worker.js')
+        .register(window.siteRoot + '/js/service-worker.js')
         .then(function () { console.log('Service Worker Registered'); });
 }
